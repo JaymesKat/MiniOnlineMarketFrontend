5196c15140dcb561cedc2a7f9b54514a1eb54f1b
S613414 <91990524+S613414@users.noreply.github.com>
2021-12-15 13:07:01 -0600

    fetch list of products

--- src/components/shared/navbar/NavBar.js
+++ src/components/shared/navbar/NavBar.js
@@ -1,19 +1,27 @@
 import { Link } from 'react-router-dom';
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { BsFillCartCheckFill } from 'react-icons/bs';
 
-function NavBar(){
+function NavBar() {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-            <LinkContainer to="/"><Navbar.Brand>Mini Market</Navbar.Brand></LinkContainer>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <LinkContainer to="/"><Navbar.Brand>Mini Market</Navbar.Brand></LinkContainer>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav>
                         <LinkContainer to="/login"><Nav.Link>Login</Nav.Link></LinkContainer>
                         <LinkContainer to="/register">
                             <Nav.Link>
                                 Register
+                            </Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                    <Nav class="d-flex ms-auto order-5">
+                        <LinkContainer to="/">
+                            <Nav.Link>
+                                <BsFillCartCheckFill size={30}/>
                             </Nav.Link>
                         </LinkContainer>
                     </Nav>
--- src/components/views/product-view/ProductView.js
+++ src/components/views/product-view/ProductView.js
@@ -1,11 +1,38 @@
-function ProductView(){
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+
+function ProductView() {
+    let params = useParams();
+    let [product, setProduct] = useState({});
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/v1/products/' + params.productId)
+            .then(res => {
+                setProduct(res.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }, []);
+
     return (
-        <Container>
-            <Row>
-                <Col>ProductView</Col>
-            </Row>
-        </Container>
+        <Card style={{ width: '28rem' }}>
+            <Card.Img variant="top" src="https://via.placeholder.com/100x100" />
+            <Card.Body>
+                <Card.Title>{product.title}</Card.Title>
+                <Card.Text>
+                    <p>Code: {product.code}</p>
+                    <p>Price: {product.price}</p>
+                    <p>Added on: {product.dateCreated}</p>
+                    <p>Description: {product.description}</p>
+                </Card.Text>
+                <Button variant="primary">Add To Cart</Button>
+            </Card.Body>
+        </Card>
+
     )
 }
 
-export default ProductEdit;
+export default ProductView;
